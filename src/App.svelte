fb596014a9d337b1b1c0d29d0b36093e7575a737
Yves <yves.cheung.88@gmail.com>
2023-08-30 23:22:21 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Game from "./lib/Game.svelte";
+  import Menu from "./lib/Menu.svelte";
+
+  let currentPage = "menu";
+  function startSolo() {
+    currentPage = "gameSolo";
+  }
+  function startDuo() {
+    currentPage = "gameDuo";
+  }
+  function stop() {
+    currentPage = "menu";
+  }
+</script>
+
+{#if currentPage === "menu"}
+  <Menu />
+  <button on:click={startSolo}> Start-solo </button>
+  <button on:click={startDuo}> Start-duo</button>
+{/if}
+{#if currentPage === "gameSolo"}
+  <Game mode="solo" />
+  <button on:click={stop}> Stop </button>
+{/if}
+{#if currentPage === "gameDuo"}
+  <Game mode="duo" />
+  <button on:click={stop}> Stop </button>
+{/if}
---
+++ src/lib/Game.svelte
@@ -0,0 +1,7 @@
+<script>
+  import logo from "../assets/images/logo.svg";
+  export let mode = "solo";
+</script>
+
+<img src={logo} alt="logo" />
+<p>mode: {mode}</p>
