214ede2ec4756b29ecc6d43ab693a8d86b93492d
Yves <yves.cheung.88@gmail.com>
2023-09-05 23:35:01 +0200

    increment for the score X, O and draw

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
-  import logo from "../assets/images/logo.svg";
+  import logo from "$assets/images/logo.svg";
+  import cross from "$assets/images/icon-x.svg";
+  import circle from "$assets/images/icon-o.svg";
+
   export let mode = "solo";
 
   let buttons = new Array(9).fill(null);
   let result = null as string | null;
   let turn = "X";
+  let playerXScore = 0; // Score du joueur X
+  let playerOScore = 0; // Score de l'ordinateur O
+  let playerDScore = 0;
 
   const winCombinations = [
     [0, 1, 2],
@@ -17,23 +23,32 @@
     [2, 4, 6],
   ];
   function setValue(i: number) {
-    buttons[i] = turn;
-    buttons = [...buttons];
-    turn = turn == "X" ? "0" : "X";
-    if (!buttons.includes(null)) {
-      result = "Match Draw";
-    } else {
-      checkWinner();
+    if (!buttons[i] && !result) {
+      buttons[i] = turn;
+      buttons = [...buttons];
+      turn = turn === "X" ? "O" : "X";
+      if (!buttons.includes(null)) {
+        result = "Match Draw";
+        playerDScore++;
+      } else {
+        checkWinner();
+      }
     }
   }
   function checkWinner() {
     for (let i = 0; i < winCombinations.length; i++) {
-      if (buttons[winCombinations[i][0]] != null) {
+      if (buttons[winCombinations[i][0]] !== null) {
         if (
-          buttons[winCombinations[i][0]] == buttons[winCombinations[i][1]] &&
-          buttons[winCombinations[i][1]] == buttons[winCombinations[i][2]]
+          buttons[winCombinations[i][0]] === buttons[winCombinations[i][1]] &&
+          buttons[winCombinations[i][1]] === buttons[winCombinations[i][2]]
         ) {
-          result = "Winner" + buttons[winCombinations[i][0]];
+          result = "Winner " + buttons[winCombinations[i][0]];
+          if (buttons[winCombinations[i][0]] === "X") {
+            playerXScore++; // Incrémenter le score du joueur X
+          } else {
+            playerOScore++; // Incrémenter le score de l'ordinateur O
+          }
+
           break;
         }
       }
@@ -57,7 +72,11 @@
           setValue(i);
         }}
       >
-        {button ? button : ""}
+        {#if button === "X"}
+          <img src={cross} alt="cross" />
+        {:else if button === "O"}
+          <img src={circle} alt="circle" />
+        {/if}
       </button>
     {/each}
   </div>
@@ -68,16 +87,84 @@
   </div>
 {/if}
 
+<div class="rectangles">
+  <div class="rectangle">
+    <div class="player-info">
+      <span class="player-text">X (YOU)</span>
+      <div class="score">Score: {playerXScore}</div>
+    </div>
+  </div>
+
+  <div class="rectangle">
+    <span class="player-text">TIES</span>
+    <div class="score">{playerXScore}</div>
+    <div class="score">Score: {playerDScore}</div>
+  </div>
+
+  <div class="rectangle">
+    <div class="player-info">
+      <span class="player-text">O (CPU)</span>
+      <div class="score">Score: {playerOScore}</div>
+    </div>
+  </div>
+</div>
+
 <style>
   .tictac {
-    width: 300px;
-    height: 300 px;
-    display: flex;
-    flex-wrap: wrap;
+    width: 420px; /* Largeur pour accueillir trois boutons de 140px chacun */
+    display: grid;
+    grid-template-columns: repeat(
+      3,
+      140px
+    ); /* Trois colonnes de 140px chacune */
+    gap: 10px; /* Espace entre les boutons */
+    justify-content: center; /* Centrer la grille horizontalement */
+    align-content: center; /* Centrer la grille verticalement */
   }
+
   .tictac button {
-    width: 100px;
-    height: 100 px;
-    margin: 0px;
+    width: 140px;
+    height: 140px;
+    flex-shrink: 0;
+    border-radius: 15px;
+    background: var(--Semi-Dark-Navy, #1f3641);
+    box-shadow: 0px -8px 0px 0px #10212a inset;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
+  /* Styles pour les rectangles */
+  .rectangles {
+    display: flex;
+    justify-content: space-between; /* Les rectangles sont espacés uniformément */
+    margin-top: 20px; /* Espace entre les boutons et les rectangles */
+  }
+
+  .rectangle {
+    width: 140px;
+    height: 72px;
+    flex-shrink: 0;
+    border-radius: 15px;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  /* Styles individuels pour chaque rectangle */
+  .rectangle:nth-child(1) {
+    background: var(--Light-Blue, #31c3bd);
+    margin-left: -10px;
+  }
+
+  .rectangle:nth-child(2) {
+    background: var(--Silver, #a8bfc9);
+  }
+
+  .rectangle:nth-child(3) {
+    background: var(--Light-Yellow, #f2b137);
+    margin-right: -10px;
+  }
+  /* faire un Test U, réassocier les boutons, afficher le résultat */
 </style>
